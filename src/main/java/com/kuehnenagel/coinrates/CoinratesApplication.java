d3d5b9c2d0b2d4e1018cbebc272edef1b86c3a2b
A1eks <a1ekskosyak@gmail.com>
2021-05-07 10:41:40 +0300

    App initialization Added Maven and Spring Boot.

---
+++ src/main/java/com/kuehnenagel/coinrates/CoinratesApplication.java
@@ -0,0 +1,13 @@
+package com.kuehnenagel.coinrates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinratesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinratesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kuehnenagel/coinrates/CoinratesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuehnenagel.coinrates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinratesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
